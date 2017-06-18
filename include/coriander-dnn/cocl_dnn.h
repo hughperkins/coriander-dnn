93492b5356f30cd6b371bd20e68cfa25c033b5e0
Hugh Perkins <hughperkins@gmail.com>
2017-06-18 22:24:30 +0100

    first draft of moving dnn to plugin

---
+++ include/coriander-dnn/cocl_dnn.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "coriander-dnn/cocl_dnn_core.h"
+#include "coriander-dnn/cocl_dnn_pooling.h"
+#include "coriander-dnn/cocl_dnn_act.h"
+#include "coriander-dnn/cocl_dnn_conv.h"
